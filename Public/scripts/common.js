ecedcb4dd89c5dc6574acf8e2588100b68c183dd
laijihua <1164258202@qq.com>
2025-07-22 23:10:42 +0800

    Feat: 优化首页

--- Public/scripts/common.js
+++ Public/scripts/common.js
@@ -6,3 +6,11 @@ function responseSuccess(data) {
     }
 
 }
+
+function loadMoreArticles() {
+    // This is a placeholder function.
+    // In a real application, you would make an AJAX request here
+    // to fetch more articles and append them to the list.
+    console.log("Load more articles button clicked!");
+    alert("加载更多文章功能待实现！");
+}
